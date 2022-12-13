a439faaacbaf77e991556e91759b3326a6478369
aryan <aryanrazwan07@gmail.com>
2022-12-13 17:48:00 +0545

    file added

--- day5.js
+++ day5.js
@@ -1,1 +1,13 @@
-console.log('I just created a git repo')
+// arrange
+let array = [1, 2, 3, 4, 5, 6, 7];
+alert(array[2]);
+// objects
+let object1 = { name: "aryan", age: 19 };
+// obj[key] or obj.key
+console.log(object1.name, "object1");
+console.log(object1["age"], "object1");
+
+let object2 = { name: "yash", height: 6};
+console.log(object2.name, "object2");
+console.log(object2["height"], "object2");
+
