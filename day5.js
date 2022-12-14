a992b34bd0524320f87c13f767edaf5029aaac31
aryan <aryanrazwan07@gmail.com>
2022-12-14 17:09:57 +0545

    essageday 6 file

--- day5.js
+++ day5.js
@@ -1,13 +1,5 @@
 // arrange
-let array = [1, 2, 3, 4, 5, 6, 7];
-alert(array[2]);
-// objects
-let object1 = { name: "aryan", age: 19 };
-// obj[key] or obj.key
-console.log(object1.name, "object1");
-console.log(object1["age"], "object1");
+let 
 
-let object2 = { name: "yash", height: 6};
-console.log(object2.name, "object2");
-console.log(object2["height"], "object2");
+
 
---
+++ day6.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "aryan",
+    age: 20,
+}
+
+if (person.age>18){
+    console.log('i am', person.name)
+}
+
+if(person.name === "aryan" && person.age>19){
+    console.log('i am', person.name, " and ", person.age)
+}
